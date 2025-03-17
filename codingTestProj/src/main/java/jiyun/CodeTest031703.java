a7fa223e42f5d789d22dbcfd65be3618cda1fe94
jiyun-choi-1217 <niyuuineedu@gmail.com>
2025-03-17 20:16:22 +0900

    jiyun_0313_03
    ww

---
+++ codingTestProj/src/main/java/jiyun/CodeTest031703.java
@@ -0,0 +1,23 @@
+package jiyun;
+
+public class CodeTest031703 {
+
+	public static void main(String[] args) {
+		/*3. 배열 뒤집기
+		•	문제: 정수 배열을 입력받아, 배열을 뒤집어 반환하는 함수를 작성하세요.
+		•	입력: [1, 2, 3, 4, 5]
+		•	출력: [5, 4, 3, 2, 1]*/
+		
+		int a[]= {1,2,3,4,5}; //원래 배열
+		
+		int b[] = {};
+		
+		for(int i = a.length; i>=0; i--) {
+			
+		}
+		
+
+
+	}
+
+}

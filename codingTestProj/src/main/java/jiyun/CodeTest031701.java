cda54ca4fb3e840c5f86e27f6b4a01d15d0accb8
jiyun-choi-1217 <niyuuineedu@gmail.com>
2025-03-17 19:14:14 +0900

    0317_01_jiyun
    2

---
+++ codingTestProj/src/main/java/jiyun/CodeTest031701.java
@@ -0,0 +1,16 @@
+package jiyun;
+
+public class CodeTest031701 {
+	
+	public static int factorial(int a) {
+		System.out.println(a);
+		int sum = 1;
+		for(int i= a; i>=1l i--)
+	}
+	public static void main(String[] args) {
+		/*
+		 * 1. 팩토리얼 계산 • 정수 n에 대해 n!을 계산하는 재귀 함수 작성. • 입력: 5 • 출력: 120
+		 */
+		
+	}
+}

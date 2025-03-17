8c198b01055928f82f17b331fbcbbdc4afad72c2
jeon10junho14 <19bnamfm@gmail.com>
2025-03-17 20:21:03 +0900

    asdf

---
+++ codingTestProj/src/main/java/Junho/j3rd.java
@@ -0,0 +1,25 @@
+package Junho;
+
+public class junho_3rd_0317 {
+//	3. 배열 뒤집기
+//	•	문제: 정수 배열을 입력받아, 배열을 뒤집어 반환하는 함수를 작성하세요.
+//	•	입력: [1, 2, 3, 4, 5]
+//	•	출력: [5, 4, 3, 2, 1]
+	
+	int a[]= {1,2,3,4,5};
+		
+	public static void main(String[] args) {
+		junho_3rd_0317 j30 = new junho_3rd_0317();
+		j30.
+	}
+	
+	public void turn(int a[]) {
+		
+		for(int i =0; i <= a.length; i++) {
+			for(int j=0; j <= a.length; j++) {
+				System.out.println();
+			}
+		}
+	}
+
+}//class
